df15286d8ab0388f43d46aa7ccd7b80a8d1eb003
samples28 <ct_student@163.com>
2021-01-28 09:38:54 +0800

    Update main.go

--- main.go
+++ main.go
@@ -8,6 +8,6 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, Kubernetes！I'm from Jenkins CI！")
+	fmt.Println("Hello, 2021 Kubernetes！I'm from Jenkins CI！")
 	fmt.Println("BRANCH_NAME:", os.Getenv("branch"))
 }
